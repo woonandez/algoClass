1e64bb0d57dd34d94bf15e91f35432831601447b
woonandez <matthew.allen.fernandez@gmail.com>
2017-08-23 14:42:19 -0700

    implement reverse string function

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -5,7 +5,7 @@ factorial(5) => 5*4*3*2*1 => 120
 */
 
 function computeFactorial(num) {
-  if (num === 1) {
+   if (num === 1) {
     return num;
   } else {
     return num * computeFactorial(num - 1);
--- recursion/reverse.js
+++ recursion/reverse.js
@@ -4,3 +4,13 @@ Implement a function that will reverse a string recursively.
 reverse('abcdefg')
 => 'gfedcba'
 */
+function reverse(str, index = str.length - 1) {
+  if (index < 0) {
+    return str;
+  } else {
+    let arr = str.split('');
+    arr.push(arr[index]);
+    arr.splice(index, 1)
+    return reverse(arr.join(''), index - 1);
+  }
+}
