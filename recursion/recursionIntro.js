73a54f6cbd0f13575bcf6850980a87b24ddeb720
woonandez <matthew.allen.fernandez@gmail.com>
2017-08-23 13:53:07 -0700

    implement countdown func using recursion

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -17,7 +17,7 @@ function countDown(n) {
 }
 
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
-function countDown(base, expo) {
+function exponent(base, expo) {
   let baseResult = base;
   while(expo) {
     baseResult = baseResult * base;
@@ -27,6 +27,13 @@ function countDown(base, expo) {
 }
 
 //4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
+function countDown(base, expo) {
+  if (expo === 0) {
+    return base;
+  } else {
+    return base * countDown(base, expo - 1);
+  }
+}
 
 //5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
 
